3cf432af7257d46cdee69a62d494460f6863edbe
Marcell Kovács <marcell.kovacs123@gmail.com>
2025-01-20 21:44:17 +0100

    0120 haladás

--- backend/roman_map/serializers.py
+++ backend/roman_map/serializers.py
@@ -1,13 +1,30 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Territorie, Historie, CustomPolygon, CustomPoint
 from rest_framework import serializers
+import json
+
+class CoordinatesConvert(serializers.Field):
+    def to_representation(self, value):
+        try:
+            return json.loads(value)
+        except:
+            raise serializers.ValidationError("Serializer hiba: A JSON formátum nem valid.")
+    
+    def to_internal_value(self, data):
+        try:
+            return json.dump(data)
+        except:
+            raise serializers.ValidationError("Serializer hiba: A JSON formátum nem valid.")
+
 
 class TerritorieSerializer(serializers.ModelSerializer):
+    coordinates = CoordinatesConvert()
     class Meta:
         model = Territorie
-        fields = '__all__'
+        fields = ['id', 'name', 'start_date', 'end_date', 'color', 'coordinates']
 
 class HistorieSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Historie
         fields = '__all__'
