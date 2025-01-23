1a9301626afeaade830334fbbc6aaf99eb02851f
Marcell Kovács <marcell.kovacs123@gmail.com>
2025-01-23 11:57:10 +0100

    0123 api kérés és kirajzolás, színek

--- backend/roman_map/static/scripts/openlayers_api_test.js
+++ backend/roman_map/static/scripts/openlayers_api_test.js
@@ -1,21 +1,21 @@
-// Alaptérkép létrehozása
+
 const map = new ol.Map({
     target: 'map',
     layers: [
         new ol.layer.Tile({
-            source: new ol.source.OSM() // Alaptérkép (OpenStreetMap)
+            source: new ol.source.OSM() 
         })
     ],
     view: new ol.View({
-        center: ol.proj.fromLonLat([0, 0]), // Alapértelmezett középpont
+        center: ol.proj.fromLonLat([0, 0]), 
         zoom: 2
     })
 });
 
-// GeoJSON réteg létrehozása
+
 const vectorSource = new ol.source.Vector({
     format: new ol.format.GeoJSON(),
-    url: 'http://127.0.0.1:8000/territories/' // API végpont
+    url: 'http://127.0.0.1:8000/territories/' 
 });
 
 const styleFunction = (feature) => {
@@ -24,21 +24,21 @@ const styleFunction = (feature) => {
     color[3] = 0.5;
     return new ol.style.Style({
         stroke: new ol.style.Stroke({
-            color: 'black', // Körvonal színe
+            color: 'black', 
             width: 2
         }),
         fill: new ol.style.Fill({
-            color: color // Feature saját színe
+            color: color 
         })
     });
 };
 
-// Stílus a MultiPolygonokhoz
+
 const vectorLayer = new ol.layer.Vector({
     source: vectorSource,
     style: styleFunction,
     
 });
 
-// Réteg hozzáadása a térképhez
+
 map.addLayer(vectorLayer);
