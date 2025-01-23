f58dde1f47d60960fb0b36766463dab77926e38c
Marcell Kovács <marcell.kovacs123@gmail.com>
2025-01-23 23:08:05 +0100

    kijelölés a polygonokon, adatok kiírása a polygon felett.

--- backend/roman_map/static/scripts/openlayers_api_test.js
+++ backend/roman_map/static/scripts/openlayers_api_test.js
@@ -21,11 +21,11 @@ const vectorSource = new ol.source.Vector({
 const styleFunction = (feature) => {
     
     let color = ol.color.asArray(feature.get('color')).slice();
-    color[3] = 0.5;
-    let strokeColor = color
+    color[3] = 0.0;
+    let strokeColor = 'rgba(0, 0, 0, 0.0)'
     if(feature.get('hidden')){
-        color[3] = 0.0;
-        strokeColor = 'rgba(0, 0, 0, 0.0)'
+        color[3] = 0.5;
+        strokeColor = 'rgba(0, 0, 0, 1)'
     }
     return new ol.style.Style({
         stroke: new ol.style.Stroke({
@@ -38,7 +38,6 @@ const styleFunction = (feature) => {
     });
 };
 
-
 const vectorLayer = new ol.layer.Vector({
     source: vectorSource,
     style: styleFunction,
@@ -47,19 +46,95 @@ const vectorLayer = new ol.layer.Vector({
 
 map.addLayer(vectorLayer);
 
-document.getElementById('date-slider').addEventListener('input', function (event){
-    let selectedDate = parseInt(event.target.value)
+
+function filterByDate(selectedDate){
     document.getElementById('slider-value').textContent = selectedDate;
 
     vectorSource.forEachFeature((feature) => {
         let stard_date = feature.get('start_date')
         let end_date = feature.get('end_date')
         if(selectedDate < stard_date || selectedDate >= end_date){
-            feature.set('hidden', true);
-        }else{
             feature.set('hidden', false);
+        }else{
+            feature.set('hidden', true);
         }
 
         feature.changed()
     })
-})
+}
+document.getElementById('date-slider').addEventListener('input', function (event){
+    let selectedDate = parseInt(event.target.value)
+    filterByDate(selectedDate);   
+});
+
+// selecting
+
+function capitalizeFirstCharacters(value){
+    return String(value).charAt(0).toUpperCase() + String(value).slice(1);
+}
+
+function handleDates(value){
+    let number = parseInt(value);
+    if(!isNaN(number)){
+        
+        if(number <= 0){
+            return 'Kr. e. '+String(Math.abs(number));
+        }
+        else{
+            return 'Kr. u. '+String(number);
+        }
+    }
+    else{
+        return value
+    }
+}
+
+let select = null;
+function selectStyle(feature) {
+    let color = ol.color.asArray(feature.get('color')).slice();
+    color[3] = 0.0;
+    let strokeColor = 'rgba(0, 0, 0, 0.0)';
+
+    let featureName = capitalizeFirstCharacters(feature.get('name')) || 'N/A';
+    let start_date = handleDates(feature.get('start_date') || 'N/A');
+    let end_date = handleDates(feature.get('end_date') || 'N/A');
+
+
+    if(feature.get('hidden')){
+        color[3] = 0.5;
+        strokeColor = 'rgba(255, 255, 255, 0.7)'
+    }
+    return new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: color, 
+        }),
+        stroke: new ol.style.Stroke({
+            color: strokeColor,
+            width: 2,
+        }),
+        text: new ol.style.Text({
+            font: '14px Arial',
+            fill: new ol.style.Fill({ color: '#000' }),
+            stroke: new ol.style.Stroke({ color: '#fff', width: 2 }),
+            text: `${featureName}\n${start_date} - ${end_date}`,
+            offsetY: -10,
+        }),
+    });
+}
+
+const selectSingleClick = new ol.interaction.Select({
+    style: selectStyle,
+});
+
+function changeInteraction() {
+    if (select !== null) {
+        map.removeInteraction(select);
+    }
+    select = selectSingleClick;
+    map.addInteraction(select);
+}
+
+changeInteraction();
+
+
+
