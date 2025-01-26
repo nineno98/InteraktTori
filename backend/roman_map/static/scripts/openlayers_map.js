6e5efa1caf2790bf34f0e90078d4d2604ca2b664
Marcell Kovács <marcell.kovacs123@gmail.com>
2025-01-26 17:54:19 +0100

    rajzolásnál működik az osszes típus, kijelölés és módosítás működik.

--- backend/roman_map/static/scripts/openlayers_map.js
+++ backend/roman_map/static/scripts/openlayers_map.js
@@ -2,11 +2,11 @@ const hatter = new ol.layer.Tile({
     source: new ol.source.OSM(),
   });
   
-const drawingSource = new ol.source.Vector({wrapX: false});
+//const drawingSource = new ol.source.Vector({wrapX: false});
   
-const drawingLayer = new ol.layer.Vector({
+/*const drawingLayer = new ol.layer.Vector({
     source: drawingSource,
-  });
+  });*/
 
 
 class TerritoriesVectorLayer{
@@ -30,19 +30,16 @@ class TerritoriesVectorLayer{
     }
 
     SetStyleFunction(feature){
-        //let color = ol.color.asArray(feature.get('color')).slice();
-        //let color = feature.get('color');
-        //console.log(color);
-        //color[3] = 0.7;
         
-
         var hexColor = feature.get('color');
         var color = ol.color.asArray(hexColor);
         color = color.slice();
         color[3] = 0.7;
-        console.log(color);
-        let strokeColor = this.rgbaColorDarker(color.slice(), 0.1);
-        //console.log(strokeColor);
+        let strokeColor = this.rgbaColorDarker(color.slice(), 0.2);
+        let featureName = this.capitalizeFirstCharacters(feature.get('name')) || 'N/A';
+        let start_date = this.handleDates(feature.get('start_date') || 'N/A');
+        let end_date = this.handleDates(feature.get('end_date') || 'N/A');
+        
 
         if(feature.get('hidden')){
             return null;
@@ -55,13 +52,20 @@ class TerritoriesVectorLayer{
               color: strokeColor,
               width: 2,
             }),
+            text: new ol.style.Text({
+                font: '14px Arial',
+                fill: new ol.style.Fill({ color: '#000' }),
+                stroke: new ol.style.Stroke({ color: '#fff', width: 2 }),
+                text: `${featureName}\n${start_date} - ${end_date}`,
+                offsetY: -10,
+            }),
         });
     }
 
     filterByDate(event){
         let selectedDate = parseInt(event.target.value);
         document.getElementById('slider-value').textContent = selectedDate;
-        console.log('filtering')
+        //console.log('filtering')
         this.vectorSource.forEachFeature((feature) => {
             let stard_date = feature.get('start_date')
             let end_date = feature.get('end_date')
@@ -94,45 +98,108 @@ class TerritoriesVectorLayer{
             }
         }
         catch (error) {
-            console.error('Hiba történt a szín sötétítésekor: ', error.message);
+            console.error('rgbaColorDarker: Hiba történt a szín sötétítésekor: ', error.message);
+        } 
+    }
+    capitalizeFirstCharacters(value){
+        try{
+            if(typeof value == 'string')
+                return String(value).charAt(0).toUpperCase() + String(value).slice(1);
+            else
+                throw new Error("A value érték nem string")
+        }catch (error) {
+            console.error('capitalizeFirstCharacters: Hiba:'+error.message);
+        }
+    }
+    handleDates(value){
+        try{
+            let number = parseInt(value);
+            if(!isNaN(number)){     
+                if(number <= 0){
+                    return 'Kr. e. '+String(Math.abs(number));
+                }
+                else{
+                    return 'Kr. u. '+String(number);
+                }
+            }
+            else{
+                return value
+            }
+        }catch (error) {
+            console.error('handleDates: Hiba:'+error.message);
         }
         
     }
 
 }
 
-const territories = new TerritoriesVectorLayer('http://127.0.0.1:8000/territories/');
 
-
-  const map = new ol.Map({
-    layers: [
-        hatter,
-        territories.getLayer(),
-        drawingLayer,
-        
-
-    ],
-    target: 'map',
-    view: new ol.View({
-      center: ol.proj.fromLonLat([12.496366, 41.902782]),
-      zoom: 4,
-      projection: 'EPSG:3857',
-    }),
-  });
   
   class HandleDraw {
-    constructor(){
+    constructor(apiUrl){
+        this.apiUrl = apiUrl;
+        this.drawenabled = false;
+        this.selectenabled = false;
+        this.select = null;
+        this.draw = null;
+        this.modify = null;
+
+        this.selectedStyle = new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(233,84,33,0.5)', 
+            }),
+            stroke: new ol.style.Stroke({
+                color: 'blue',
+                width: 2,
+            }),
+        })
+
+        this.drawingSource = new ol.source.Vector({
+            wrapX: false,
+            format: new ol.format.GeoJSON(),
+            url: this.apiUrl,
+
+        });
+  
+        this.drawingLayer = new ol.layer.Vector({
+            source: this.drawingSource,
+        });
+
         this.undoButton = document.getElementById('undo');
         this.undoButton.addEventListener('click', this.undoHandling.bind(this), false);
+
         this.typeSelect = document.getElementById('type');
         this.typeSelect.addEventListener('change', this.changeHandle.bind(this), false);
-        this.draw = null;
+
+        this.drawenableButton = document.getElementById('draw-enable');
+        this.drawenableButton.addEventListener('click', this.enableDraw.bind(this), false);
+
+        this.selectenableButton = document.getElementById('select-enable');
+        this.selectenableButton.addEventListener('click', this.enableSelect.bind(this), false);
+
+        
+
+        
     }
+
+
+    addSelect(){
+        this.select = new ol.interaction.Select({
+            layers: [this.drawingLayer],
+            style: this.selectedStyle,
+        });
+        map.addInteraction(this.select);
+        this.modify = new ol.interaction.Modify({
+            features: this.select.getFeatures()
+        });
+        map.addInteraction(this.modify);
+    }
+
     addInteraction() {
         const value = this.typeSelect.value;
         if (value !== 'None') {
           this.draw = new ol.interaction.Draw({
-            source: drawingSource,
+            source: this.drawingSource,
             type: this.typeSelect.value,
           });
           map.addInteraction(this.draw);
@@ -150,10 +217,74 @@ const territories = new TerritoriesVectorLayer('http://127.0.0.1:8000/territorie
     turnOffDaw(){
         map.removeInteraction(this.draw);
     }
+
+    getLayer(){
+        return this.drawingLayer;
+    }
+
+    turnOnDraw(){
+        this.addInteraction();
+    }
+
+    enableDraw(){
+        if(!this.drawenabled){
+            if(this.selectenabled){
+                map.removeInteraction(this.select);
+                map.removeInteraction(this.modify);
+                this.selectenabled = false;
+            }
+            this.drawenabled = true;
+            this.turnOnDraw();
+            
+        }
+        else{
+            this.drawenabled = false;
+            this.turnOffDaw();
+        }
+    }
+
+    enableSelect(){
+        if(!this.selectenabled){
+            if(this.drawenabled){
+                this.drawenabled = false;
+                this.turnOffDaw();
+            }
+            this.selectenabled = true;
+            this.addSelect();
+            
+        }
+        else{
+            this.selectenabled = false;
+            map.removeInteraction(this.select);
+            map.removeInteraction(this.modify);
+        }
+    }
   }
 
-  const handledraw = new HandleDraw();
-  handledraw.addInteraction();
+
+
+  const territories = new TerritoriesVectorLayer('http://127.0.0.1:8000/territories/');
+const drawing = new HandleDraw('http://127.0.0.1:8000/custompolygons/')
+
+  const map = new ol.Map({
+    layers: [
+        hatter,
+        territories.getLayer(),
+        drawing.getLayer(),
+        
+
+    ],
+    target: 'map',
+    view: new ol.View({
+      center: ol.proj.fromLonLat([12.496366, 41.902782]),
+      zoom: 4,
+      projection: 'EPSG:3857',
+    }),
+  });
+
+
+  /*const handledraw = new HandleDraw();
+  handledraw.addInteraction();*/
 
   /*const typeSelect = document.getElementById('type');
   
